7cd24b72ecc39dc073700ba9f3d1bba3014ce815
Allan DEMARBRE <demarbreallan.dev@gmail.com>
2023-08-08 18:53:22 +0200

    Script to build a dictionnary of Pokémon names

--- src/scripts/build-dict.js
+++ src/scripts/build-dict.js
@@ -1,8 +1,15 @@
 import ASCIIFolder from "fold-to-ascii";
+import fs from "fs";
+import { join } from "path";
 
 async function buildDictionnary() {
+  console.log("Fetching data...");
   const data = await fetchData();
+  console.log("Data fetched successfully");
 
+  const pokemonNameList = [];
+
+  console.log("Parsing data...");
   for (let row of data) {
     let name = row.name.fr;
 
@@ -11,9 +18,18 @@ async function buildDictionnary() {
     name = name.toLowerCase();
 
     if (!name.match(/^[a-z]+$/)) {
-      console.log(name, "is not valid");
+      console.log(name, "is not valid, skipped");
+      continue;
     }
+
+    pokemonNameList.push(name);
   }
+  console.log("Data parsed");
+
+  console.log("Writing data...");
+  const outputPath = join("src", "assets", "pokemon-names.json");
+  fs.writeFileSync(outputPath, JSON.stringify(pokemonNameList, null, 2));
+  console.log("Data written to :", outputPath);
 }
 
 async function fetchData() {
