c364b174977e7ab13c034c1f9b98ddbbbebdc7ba
Allan Demarbre <ademarbre@groupe-cac.com>
2023-10-23 16:54:07 +0200

    Added colors, updated readme

--- tools/constants.js
+++ tools/constants.js
@@ -2,3 +2,7 @@ import { join } from "path";
 
 export const POKEMON_DATA_PATH = join("..", "public", "pokemon-data.json");
 export const POKEAPI_URL = "https://api-pokemon-fr.vercel.app/api/v1/pokemon";
+
+export const SEVERITY_INFO = "INFO";
+export const SEVERITY_WARN = "WARN";
+export const SEVERITY_ERROR = "ERROR";
